080b1899cc6114829b20a47fdfc0e735923e589a
Daichi <a5822108@aoyama.jp>
2024-10-16 18:15:08 +0900

    40: commonの修正

--- app/(protected)/ems/common/page.tsx
+++ app/(protected)/ems/common/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const CommonPage = () => {
     return (
         <div className='bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
-            from-sky-400 to-blue-800 h-full'>
+            from-sky-400 to-blue-800 min-h-full pb-3'>
             <Header />
             <div className="bg-[#F5F5F8] mx-2 rounded-lg mb-3 py-2 px-2 shadow-md">
                 <p className='text-xl'>共通予約カレンダー</p>
