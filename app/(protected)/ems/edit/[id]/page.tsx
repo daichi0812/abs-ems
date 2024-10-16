a3461c0b311bf5f0d46209f5e76d3db04e2af697
a5822125 <a5822125@aoyama.jp>
2024-10-16 16:09:27 +0900

    fix

--- app/(protected)/ems/edit/[id]/page.tsx
+++ app/(protected)/ems/edit/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState, useEffect, useRef, useTransition } from 'react';
 import { useParams, useRouter } from 'next/navigation';
@@ -112,95 +112,6 @@ const EditPage = () => {
   };
 
   return (
-    <div className="bg-[#F5F5F8] shadow rounded-md p-3 mt-3 mx-2">
-      <p className='text-xl mb-1'>機材情報の編集</p>
-      <label
-        htmlFor="imageInput"
-        className="mb-2"
-        style={{
-          border: "white 3px dotted",
-          width: FIELD_SIZE,
-          height: FIELD_SIZE,
-          display: "flex",
-          borderRadius: 12,
-          justifyContent: "center",
-          alignItems: "center",
-          overflow: "hidden",
-          cursor: "pointer",
-        }}
-      >
-        {(imageUrl && imageFile) ? (
-          <img
-            src={imageUrl}
-            alt="アップロード画像"
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
-          />
-        ) : equipmentImg ? (
-          <img
-            src={equipmentImg}
-            alt="現在の画像"
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
-          />
-        ) : (
-          "+ 画像をアップロード"
-        )}
-        <InputImage
-          ref={inputFileRef}
-          id="imageInput"
-          onChange={handleFileChange}
-        />
-      </label>
-      <input
-        className='mb-1 rounded-md px-1'
-        type="text"
-        placeholder="機材名を入力してください"
-        style={{ border: '1px solid black' }}
-        value={equipmentName}
-        onChange={(e) => setEquipmentName(e.target.value)}
-      />
-      <textarea
-        className="rounded-md px-1"
-        placeholder="説明文を入力してください"
-        style={{ width: '100%', height: '100px', border: '1px solid black' }}
-        value={equipmentDetail}
-        onChange={(e) => setEquipmentDetail(e.target.value)}
-      />
-      <div className="flex gap-2">
-
-        {isPending_1 ? (
-          <Button
-            isLoading
-            colorScheme='blue'
-          >
-            更新
-          </Button>
-        ) : (
-          <Button
-            disabled={isPending_1}
-            onClick={() => startTransition_1(() => handleUpdateEquipment())}
-            colorScheme='blue'
-          >
-            更新
-          </Button>
-        )
-        }
-         {isPending_2 ? (
-          <Button
-            isLoading
-            colorScheme='yellow'
-          >
-              キャンセル
-          </Button>
-        ) : (
-          <Button
-            disabled={isPending_2}
-            onClick={() => startTransition_2(() => handleClickCancelButton())}
-            colorScheme='yellow'
-          >
-            キャンセル
-          </Button>
-        )
-        }
     <div>
       <Header />
       <div className="bg-[#F5F5F8] shadow rounded-md p-3 mt-3 mx-2">
@@ -257,32 +168,42 @@ const EditPage = () => {
           onChange={(e) => setEquipmentDetail(e.target.value)}
         />
         <div className="flex gap-2">
-          <Button
-            onClick={handleUpdateEquipment}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: '#00bfff',
-              color: 'white',
-              fontSize: '16px',
-              borderRadius: '5px',
-              cursor: 'pointer',
-              width: '80px',
-              height: '40px'
-            }}
-          >
-            更新
-          </Button>
-          <Button
-            onClick={handleClickCancelButton}
-            style={{ backgroundColor: '#f5b942', color: 'white', fontSize: '16px', borderRadius: '5px', cursor: 'pointer', width: '95px', height: '40px' }}>
-            キャンセル
-          </Button>
+          {isPending_1 ? (
+            <Button
+              isLoading
+              colorScheme='blue'
+            >
+              更新
+            </Button>
+          ) : (
+            <Button
+              disabled={isPending_1}
+              onClick={() => startTransition_1(() => handleUpdateEquipment())}
+              colorScheme='blue'
+            >
+              更新
+            </Button>
+          )}
+          {isPending_2 ? (
+            <Button
+              isLoading
+              colorScheme='yellow'
+            >
+              キャンセル
+            </Button>
+          ) : (
+            <Button
+              disabled={isPending_2}
+              onClick={() => startTransition_2(() => handleClickCancelButton())}
+              colorScheme='yellow'
+            >
+              キャンセル
+            </Button>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default EditPage
+export default EditPage;
