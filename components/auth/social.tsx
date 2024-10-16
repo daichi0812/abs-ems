fc6f0eb0d42b2bbc082d82e62a613aaa9a0f68ee
a5822125 <a5822125@aoyama.jp>
2024-10-16 18:41:07 +0900

    ログイン画面のボタンローディングを追加しました。

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -6,67 +6,55 @@ import { FaGithub } from "react-icons/fa"
 import { useSearchParams } from "next/navigation"
 
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
-import { useTransition } from "react"
+import { useState } from "react"
 import { Button } from "@chakra-ui/react"
 
 export const Social = () => {
     const searchParams = useSearchParams();
     const callbackUrl = searchParams.get("callbackUrl");
 
-    const onClick = (provider: "google" | "github") => {
-        signIn(provider, {
-            callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
-        });
-    }
+    const [isLoadingGoogle, setIsLoadingGoogle] = useState(false);
+    const [isLoadingGithub, setIsLoadingGithub] = useState(false);
 
-    const [isPending_1, startTransition_1] = useTransition();
-    const [isPending_2, startTransition_2] = useTransition();
+    const onClick = async (provider: "google" | "github") => {
+        if (provider === "google") {
+            setIsLoadingGoogle(true);
+        } else {
+            setIsLoadingGithub(true);
+        }
+
+        try {
+            await signIn(provider, {
+                callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
+            });
+        } finally {
+            setIsLoadingGoogle(false);
+            setIsLoadingGithub(false);
+        }
+    };
 
     return (
         <div className="flex items-center w-full gap-x-2">
-            {isPending_1 ? (
-                <Button
-                    isLoading
-                    size="lg"
-                    className="w-full"
-                    variant="outline"
-                    onClick={() => startTransition_1(() => onClick("google"))}
-                >
-                    <FcGoogle className="h-5 w-5" />
-                </Button>
-            ) : (
-                <Button
-                    disabled={isPending_1}
-                    size="lg"
-                    className="w-full"
-                    variant="outline"
-                    onClick={() => startTransition_1(() => onClick("google"))}
-                >
-                    <FcGoogle className="h-5 w-5" />
-                </Button>
-            )}
-            {isPending_2 ? (
-                <Button
-                    isLoading
-                    size="lg"
-                    className="w-full"
-                    variant="outline"
-                    onClick={() => startTransition_2(() => onClick("github"))}
-                >
-                    <FaGithub className="h-5 w-5" />
-                </Button>
-            ) : (
-                <Button
-                    disabled={isPending_2}
-                    size="lg"
-                    className="w-full"
-                    variant="outline"
-                    onClick={() => startTransition_2(() => onClick("github"))}
-                >
-                    <FaGithub className="h-5 w-5" />
-                </Button>
-            )}
-
+            <Button
+                isLoading={isLoadingGoogle}
+                size="lg"
+                className="w-full"
+                variant="outline"
+                onClick={() => onClick("google")}
+                disabled={isLoadingGoogle}
+            >
+                <FcGoogle className="h-5 w-5" />
+            </Button>
+            <Button
+                isLoading={isLoadingGithub}
+                size="lg"
+                className="w-full"
+                variant="outline"
+                onClick={() => onClick("github")}
+                disabled={isLoadingGithub}
+            >
+                <FaGithub className="h-5 w-5" />
+            </Button>
         </div>
-    )
-}
+    );
+};
