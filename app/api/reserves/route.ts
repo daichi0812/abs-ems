25cc1ac48102482333336bf0fa69182c54c99a62
uNiteD <136787270+daichi0812@users.noreply.github.com>
2024-12-06 14:33:23 +0900

    Revert "66: 予約機能の修正"

--- app/api/reserves/route.ts
+++ app/api/reserves/route.ts
@@ -16,35 +16,8 @@ export async function POST(request: Request) {
         const data = await request.json();
         const { user_id, list_id, start, end, isRenting } = data;
 
-        if (!user_id || !list_id || !start || !end) {
-            throw new Error('Missing required fields');
-        }
-
-        // UTC日付として解釈
-        const startDate = new Date(start); // `start`が文字列の場合、ここで正しいフォーマットか確認
-        const startDateTime = new Date(Date.UTC(
-            startDate.getUTCFullYear(),
-            startDate.getUTCMonth(),
-            startDate.getUTCDate(), // UTCの日付を取得
-            0, 0, 0, 0 // 時間をUTCの00:00:00に設定
-        ));
-
-        const endDate = new Date(end);
-        const endDateTime = new Date(Date.UTC(
-            endDate.getUTCFullYear(),
-            endDate.getUTCMonth(),
-            endDate.getUTCDate(),
-            0, 0, 0, 0
-        ));
-
         const reserve = await db.reserve.create({
-            data: {
-                user_id,
-                list_id: Number(list_id),
-                start: startDateTime,
-                end: endDateTime,
-                isRenting: isRenting || 0,
-            },
+            data: { user_id, list_id, start, end, isRenting },
         });
 
         return NextResponse.json(reserve, { status: 201 });
